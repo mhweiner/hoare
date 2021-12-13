04e52cc6b03444b606bf38257162b39c8701ef0d
Marc H. Weiner <mhweiner234@gmail.com>
2021-12-12 20:06:18 -0500

    chore!: removed undocumented assertions
    BREAKING CHANGE: Removed undocumented `truthy()`, `pass()` and `fail()` assertions. They were left over from the development process and should not be used. Part of what makes hoare unique is its radically simplified assertion API. We maintain that every assertion should use equal() or similar.

--- src/assertions/index.ts
+++ src/assertions/index.ts
@@ -5,9 +5,6 @@ import {equal} from './equal';
 export function createAssertionPredicates(assertions: Assertion[]) {
 
     return {
-        pass: (description?: string) => assertions.push({pass: true, description}),
-        fail: (description?: string) => assertions.push({pass: false, description}),
-        truthy: (statement: any, description?: string) => assertions.push({pass: !!statement, description}),
         equal: (actual: any, expected: any, description?: string) => (
             equal(assertions, actual, expected, description)
         ),
