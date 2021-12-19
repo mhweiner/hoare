5eae787973c7427c004ab851055bc938931bc88a
Marc H. Weiner <mhweiner234@gmail.com>
2021-12-19 01:37:13 -0500

    fix: issue where an object and non-object could throw error in deepEqual comparison

--- src/deepStrictEqual.ts
+++ src/deepStrictEqual.ts
@@ -2,8 +2,8 @@ export function deepStrictEqual(obj1: any, obj2: any) {
 
     if (obj1 === obj2) return true;
     if (isPrimitive(obj1) && isPrimitive(obj2)) return obj1 === obj2;
-    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
     if (typeof obj1 !== typeof obj2) return false;
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
 
     // compare objects with same number of keys
     for (const key in obj1) {
