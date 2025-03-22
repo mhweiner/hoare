e65b11ebe0e0fb547c4888f226581da819a1c824
Marc H. Weiner <mhweiner234@gmail.com>
2025-03-22 18:06:40 -0400

    chore: changed some tests to use new assertion predicates

--- src/isTestPassing.spec.ts
+++ src/isTestPassing.spec.ts
@@ -5,35 +5,35 @@ import {serializeError} from 'serialize-error';
 
 test('isTestPassing()', (assert) => {
 
-    assert.equal(isTestPassing({
+    assert.isTrue(isTestPassing({
         description: 'test',
         assertions: [
             {pass: true, description: 'assertion 1'},
             {pass: true, description: 'assertion 2'},
             {pass: true, description: 'assertion 3'},
         ],
-    }), true, 'test passes if all assertions pass');
+    }), 'test passes if all assertions pass');
 
-    assert.equal(isTestPassing({
+    assert.isFalse(isTestPassing({
         description: 'test',
         assertions: [
             {pass: true, description: 'assertion 1'},
             {pass: false, description: 'assertion 2'},
             {pass: true, description: 'assertion 3'},
         ],
-    }), false, 'test fails if one or more assumptions fail');
+    }), 'test fails if one or more assumptions fail');
 
-    assert.equal(isTestPassing({
+    assert.isFalse(isTestPassing({
         description: 'test',
         assertions: [],
-    }), false, 'test fails if no assertions');
+    }), 'test fails if no assertions');
 
-    assert.equal(isTestPassing({
+    assert.isFalse(isTestPassing({
         description: 'test',
         assertions: [
             {pass: true, description: 'assertion 1'},
         ],
         error: serializeError(new Error()),
-    }), false, 'test fails if there is an error, even if all assertions pass');
+    }), 'test fails if there is an error, even if all assertions pass');
 
 });
--- src/shouldExitWithError.spec.ts
+++ src/shouldExitWithError.spec.ts
@@ -3,33 +3,33 @@ import {shouldExitWithError} from './shouldExitWithError';
 
 test('returns false if no files without tests, and all tests pass', (assert) => {
 
-    assert.equal(shouldExitWithError({
+    assert.isFalse(shouldExitWithError({
         numFiles: 1,
         numSuccessfulTests: 5,
         numTests: 5,
         filesWithNoTests: [],
-    }), false);
+    }));
 
 });
 
 test('returns true if any files without tests', (assert) => {
 
-    assert.equal(shouldExitWithError({
+    assert.isTrue(shouldExitWithError({
         numFiles: 1,
         numSuccessfulTests: 5,
         numTests: 5,
         filesWithNoTests: ['./fake/a.spec.js'],
-    }), true, 'passes if all tests pass');
+    }), 'passes if all tests pass');
 
 });
 
 test('returns true if any tests failed', (assert) => {
 
-    assert.equal(shouldExitWithError({
+    assert.isTrue(shouldExitWithError({
         numFiles: 1,
         numSuccessfulTests: 5,
         numTests: 6,
         filesWithNoTests: [],
-    }), true);
+    }));
 
 });
